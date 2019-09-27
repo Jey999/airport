ad2ee9ab5e98457e94ee1330a67a48edbd7760d4
Vishnu Jeyarathnam <VJeyarathnam@spartaglobal.com>
2019-09-27 09:44:19 +0100

    airport iniated

---
+++ Flight.py
@@ -0,0 +1,11 @@
+class : Flight
+
+# characteristics
+
+def __init__ (self, destination, origin, time, plane, passanger_list):
+    self.destination = destination
+    self.origin= origin
+    self .time = time
+    self.plane = plane
+    self.passanger=  passanger_list
+
